b93a01d366648316bc0ae1e7698e20ba653f63d1
Joe Van Deren <jgvanderen64@gmail.com>
2022-08-10 12:09:38 -0300

    Create game with button UI and text display

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -16,13 +16,13 @@ function playRound(playChoice, compChoice) {
     let compNum = choiceToNumber(compChoice);
     let gameTime = compNum - playNum;
     if (gameTime === 0) {
-        console.log("It's a tie!");
+        results.textContent = "It's a tie!";
         return 0
     } if (gameTime === 1 || gameTime === -2) {
-        console.log(`You Lose! ${compChoice} beats ${playLowerCase}`);
+        results.textContent = `You Lose! ${compChoice} beats ${playLowerCase}`;
         return 1
     } else {
-        console.log(`You Win! ${playLowerCase} beats ${compChoice}`);
+        results.textContent = `You Win! ${playLowerCase} beats ${compChoice}`;
         return -1
     }
 }
@@ -57,5 +57,46 @@ function game() {
     }
 }
 
-console.log(game()); 
+function btnGame(pt) {
+    if (point < 0) {
+        pScore -= point;
+    } if (point > 0) {
+        cScore += point;
+    }
+    score.textContent = `Your Score: ${pScore} Computer Score: ${cScore}`;
+    if (pScore === 5) {
+        const winner = document.createElement('div');
+        winner.classList.add('winnder');
+        winner.textContent = `You Win!`
+        score.appendChild(winner);
+        cScore = 0;
+        pScore = 0; 
+    } if (cScore ===5) {
+        const winner = document.createElement('div');
+        winner.classList.add('winnder');
+        winner.textContent = `You Lose!`
+        score.appendChild(winner);
+        cScore = 0;
+        pScore = 0;
+    }
+}
 
+const results = document.querySelector('#results');
+
+document.getElementById('rock').onclick = function(){
+    point = playRound('rock', getComputerChoice());
+    btnGame(point);
+}
+
+document.getElementById('paper').onclick = function(){
+    point = playRound('paper', getComputerChoice());
+    btnGame(point);
+}
+
+document.getElementById('scissors').onclick = function(){
+    point = playRound('scissors', getComputerChoice());
+    btnGame(point);
+}
+
+let pScore = 0, cScore = 0;
+const score = document.querySelector('#score');
